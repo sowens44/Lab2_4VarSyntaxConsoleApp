38c602efca580ed2ecf6d0a0e58b2841bbf5064c
so2owens@gmail.com <so2owens@gmail.com>
2020-07-16 19:07:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lab2_4VarSyntaxConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lab 2_4 Var Syntax
+            //int myFirstNumber = 19;
+            //int mySecondNumber = 244;
+            //Console.WriteLine(myFirstNumber * mySecondNumber);
+            Console.WriteLine("Hello World2333!!");
+            int myFirstNumber = 19;
+            int mySecondNumber = 244;
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+            string mytext = "Some Text";
+            Console.WriteLine(mytext + mySecondNumber);
+            Console.WriteLine(mytext + "" + mySecondNumber);
+            // 6
+            string moreText = "Goes Here";
+            Console.WriteLine(mytext + "" + moreText);
+            Console.WriteLine($"{mytext} {moreText}");
+
+            string color1 = "Yellow";
+            string color2 = "Red";
+
+            bool isYellow = true;
+            bool isGreen = false;
+
+            Console.WriteLine(color1 + "" + isYellow + "" + color2 + "" + isGreen);
+            Console.WriteLine($"{color1} {isYellow} {color2} {isGreen}");
+
+            double numbers;
+            //12
+            numbers = mySecondNumber / myFirstNumber;
+
+            Console.WriteLine(numbers);
+            //14
+            numbers = mySecondNumber / (double)myFirstNumber;
+            //15
+            Console.WriteLine(numbers);
+
+            //16
+            Console.WriteLine("Breakpoint Here");
+            //17
+            string textToNumber = "80473";
+
+            //18
+            int results = 99 * int.Parse(textToNumber);
+            //19
+            DateTime rightNow = DateTime.Now;
+
+
+
+        }
+    }
+}
+
